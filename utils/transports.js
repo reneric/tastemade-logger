a15c4e84a69c916476dc99f5ab5974ac25a269c6
reneric <rsimmo4@gmail.com>
2019-03-05 15:22:53 -0600

    change winston import

--- utils/transports.js
+++ utils/transports.js
@@ -1,4 +1,4 @@
-const { transports, format } = require('winston');
+const winston = require('winston');
 require('winston-daily-rotate-file');
 
 /**
@@ -11,8 +11,7 @@ function getTransports ({ app_name, environment }) {
   // Define transports, files are for beats to transfer to elastic search
   // date pattern is set to have two logs per day, AM and PM
   // resulting in 14 logs every 7 days
-  // Zipping causes problems with filebeat
-  const fileTransport = new (transports.DailyRotateFile)({
+  const fileTransport = new (winston.transports.DailyRotateFile)({
     dirname: 'logs',
     filename: `${app_name}-${environment}-%DATE%.log`,
     datePattern: 'YYYY-MM-DD-A',
@@ -22,7 +21,7 @@ function getTransports ({ app_name, environment }) {
     zippedArchive: false,
   });
 
-  const consoleTransport = new transports.Console({
+  const consoleTransport = new winston.transports.Console({
     handleExceptions: true,
   });
 
